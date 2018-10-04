9e0ff1ae70760803deaaccac99724f94df8ee822
takyyy <tkytkymtd@gmail.com>
2018-10-04 23:20:39 +0900

    削除昨日完成

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -16,8 +16,8 @@
       <ul class="collection">
         <li v-for="(task, index) in tasks" v-if="!task.is_done" v-bind:id="'row_task_'
           + task.id" class="collection-item">
-         <input type="checkbox" v-on:change="doneTask(task.id, index)" v-bind:id="'task_' + task.id" />
          <label v-bind:for="'task_' + task.id">{{ task.name }}</label>
+         <button type="button" v-on:click="doneTask(task.id, index)" v-bind:id="'task_' + task.id" >done</button>
         </li>
       </ul>
     </div>
@@ -28,7 +28,6 @@
       <ul class="collection">
         <li v-for="(task, index) in finishedTasks" v-if="task.is_done" v-bind:id="'row_task_'
           + task.id" class="collection-item">
-          <input type="checkbox" v-bind:id="'task_' + task.id" checked="checked"/>
           <label v-bind:for="'task_' + task.id"  class="line-through">{{ task.name }}</label>
           <button type="button" v-on:click="removeTask(task.id, index)" v-bind:id="'task_' + task.id">Delete</button>
         </li>
@@ -50,67 +49,46 @@
     },
     mounted: function () {
       this.fetchTasks();
-      this.fetchFinishedTasks();
     },
     methods: {
       fetchTasks: function (){
         axios.get('/api/tasks').then((response) => {
           for(var i = 0; i < response.data.tasks.length; i++) {
-            this.tasks.push(response.data.tasks[i]);
+            if(response.data.tasks[i].is_done != 1){
+              this.tasks.push(response.data.tasks[i]);
+            }else{
+              this.finishedTasks.push(response.data.tasks[i]);
+            }
           }
         }, (error) => {
           console.log(error);
         });
       },
-      fetchFinishedTasks: function() {
-      axios.get('/api/tasks').then((response) => {
-        for(var i = 0; i < response.data.tasks.length; i++) {
-          if(response.data.tasks[i].is_done == 1){
-            this.finishedTasks.push(response.data.tasks[i]);
-          }
-        }
-      }, (error) => {
-        console.log(error);
-      });
-      },
       displayFinishedTasks: function() {
         document.querySelector('#finished-tasks').classList.toggle('display_none');
       },
-         createTask: function () {
-         if (!this.newTask) return;
-
-         axios.post('/api/tasks', { task: { name: this.newTask } }).then((response) => {
-           this.tasks.unshift(response.data.task);
-           this.newTask = '';
-         }, (error) => {
-           console.log(error);
-         });
+      createTask: function () {
+        if (!this.newTask) return;
+        axios.post('/api/tasks', { task: { name: this.newTask } }).then((response) => {
+          this.tasks.unshift(response.data.task);
+          this.newTask = '';
+        }, (error) => {
+          console.log(error);
+        });
       },
       doneTask: function (task_id, idx) {
-       axios.put('/api/tasks/' + task_id, { task: { is_done: 1 } }).then((response) => {
-      //   this.moveFinishedTask(task_id);
+        axios.put('/api/tasks/' + task_id, { task: { is_done: 1 } }).then((response) => {
            this.tasks.splice(idx, 1);
-       }, (error) => {
-         console.log(error);
-       });
-     },
-    //  moveFinishedTask: function(task_id) {
-    //    var el = document.querySelector('#row_task_' + task_id);
-        // DOMをクローンしておく
-    //    var el_clone = el.cloneNode(true);
-        // 未完了の方を先に非表示にする
-    //    el.classList.add('display_none');
-        // もろもろスタイルなどをたして完了済みに追加
-    //    el_clone.getElementsByTagName('label')[0].classList.add('line-through');
-    //    el_clone.getElementsByTagName('label')[0].classList.remove('word-color-black');
-    //    var li = document.querySelector('#finished-tasks > ul > li:first-child');
-    //    document.querySelector('#finished-tasks > ul').insertBefore(el_clone, li);
-    //  },
+           this.finishedTasks.unshift(response.data.task);
+        }, (error) => {
+          console.log(error);
+        });
+      },
       removeTask: function(task_id, idx){
         axios.delete('/api/tasks/' + task_id).then((response) => {
           this.finishedTasks.splice(idx, 1);
         }, (error) => {
-          console.log(error);
+           console.log(error);
         });
       }
     }
